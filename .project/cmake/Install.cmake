2f50a3ec96443f189a839ad78c66f7917332f65a
邬江 <xes@qq.com>
2025-12-26 04:56:51 +0800

    设置Install

--- .project/cmake/Install.cmake
+++ .project/cmake/Install.cmake
@@ -24,19 +24,31 @@ macro(fi_install targets)
     install(
         EXPORT ${FI_FOLDER_NAME}Targets
         FILE ${FI_FOLDER_LAST_NAME}Targets.cmake
-        DESTINATION "${CMAKE_INSTALL_LIBDIR}/${FI_FOLDER_PATH}/"
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${FI_FOLDER_PATH}/"
         NAMESPACE Fi_Imported
     )
 
     include(CMakePackageConfigHelpers)
 
-    # fi_get_sub_packages("${CMAKE_CURRENT_BINARY_DIR}" FI_SUB_PACKAGES)
+    fi_set_sub_packages(FI_SUB_PACKAGES)
 
+    configure_package_config_file(
+        "${FI_PROJECT_CMAKE_DIR}/__Config.cmake__"
+        "${CMAKE_CURRENT_BINARY_DIR}/${FI_FOLDER_LASTNAME}Config.cmake"
+        INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${FI_FOLDER_PATH}/"
+    )
 
+    write_basic_package_version_file(
+        "${CMAKE_CURRENT_BINARY_DIR}/${FI_FOLDER_LASTNAME}ConfigVersion.cmake"
+        VERSION "${FI_FOLDER_VERSION}"
+        COMPATIBILITY AnyNewerVersion
+    )
 
-
-
-
+    install(
+        FILES "${CMAKE_CURRENT_BINARY_DIR}/${FI_FOLDER_LASTNAME}Config.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${FI_FOLDER_LASTNAME}ConfigVersion.cmake"
+        DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${FI_FOLDER_PATH}/"
+    )
 endmacro()
 
 
